78d36dcb9add3e2d69c39069ca514502ab0a8539
hindinisikc <doubleflusk@gmail.com>
2025-02-15 01:28:57 +0800

    Add function to play one round

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
+
 function getHumanChoice() {
-    const playerChoice = prompt("Rock... Paper.... or Scissors...?");
+    const playerChoice = prompt("Rock... Paper.... or Scissors...?").toLowerCase();
     return playerChoice;
 }
 
-alert(getHumanChoice());
+// alert(getHumanChoice());
 
 
 
@@ -13,5 +18,39 @@ function getComputerChoice() {
     return choices[randomIndex]; // Return the random choice
 }
 
-alert(getComputerChoice());   
+// alert(getComputerChoice());   
 
+
+function playRound(humanSelection, computerSelection) {
+    if (humanSelection === computerSelection) {
+        return "It's a tie!";
+    }
+
+    if (
+        (humanSelection === "rock" && computerSelection === "scissors") ||
+        (humanSelection === "paper" && computerSelection === "rock") ||
+        (humanSelection === "scissors" && computerSelection === "paper") 
+    ) {
+        humanScore++;
+        return `You win! ${humanSelection} beats ${computerSelection}`;
+    } else {
+        computerScore++;
+        return `You lost! ${computerSelection} beats ${humanSelection}`;
+    }
+}
+
+alert(playRound(getHumanChoice(), getComputerChoice()));
+
+
+
+
+// function playGame(humanChoice, computerChoice) {
+//     for (let i = 0; i < 5; i++) {
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         alert()
+//     }
+// }
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
