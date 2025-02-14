113ba4324b5fb03c06c5a60ad1003cb88cd4106d
hindinisikc <doubleflusk@gmail.com>
2025-02-15 01:13:03 +0800

    added humanchoice

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+function getHumanChoice() {
+    const playerChoice = prompt("Rock... Paper.... or Scissors...?");
+    return playerChoice;
+}
+
+alert(getHumanChoice());
+
+
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]; // Array of choices
     const randomIndex = Math.floor(Math.random() * choices.length); // Generate a random index (0, 1, or 2)
     return choices[randomIndex]; // Return the random choice
 }
 
-alert(getComputerChoice());
+alert(getComputerChoice());   
+
